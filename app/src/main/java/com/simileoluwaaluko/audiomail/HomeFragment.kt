07a93a373cc942685869fb5781c69aa193bece64
simileoluwa-aluko <simialuko24@gmail.com>
2020-03-24 11:49:38 +0100

    done layout of home page

--- app/src/main/java/com/simileoluwaaluko/audiomail/HomeFragment.kt
+++ app/src/main/java/com/simileoluwaaluko/audiomail/HomeFragment.kt
@@ -1,13 +1,18 @@
 package com.simileoluwaaluko.audiomail
 
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import org.jetbrains.anko.support.v4.toast
 import java.util.*
 
 /**
@@ -25,7 +30,7 @@ class HomeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        textToSpeech = TextToSpeech(context, TextToSpeech.OnInitListener { status ->
+       /* textToSpeech = TextToSpeech(context, TextToSpeech.OnInitListener { status ->
                 if(status != TextToSpeech.ERROR){
                     val result = textToSpeech.setLanguage(Locale.US)
                     if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
@@ -35,14 +40,35 @@ class HomeFragment : Fragment() {
                 }else{
                     Log.e("Simi_TTS", "$status initialisation failed")
                 }
-            })
+            })*/
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+        if(intent.resolveActivity(activity!!.packageManager) != null){
+            startActivityForResult(intent, 10)
+        }else toast("doesnt support speech input").show()
+
     }
 
-    override fun onDestroy() {
+/*    override fun onDestroy() {
         if(textToSpeech != null){
             textToSpeech.stop()
             textToSpeech.shutdown()
         }
         super.onDestroy()
+    }*/
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            10 -> {
+                if(resultCode == Activity.RESULT_OK && data != null){
+                    val results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    if(results!=null) toast(results[0]).show()
+                }
+            }
+        }
     }
 }
